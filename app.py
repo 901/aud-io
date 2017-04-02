777f03502082bc690b3dc4620201f8a099fe428d
Heman Gandhi <hemangandhi@gmail.com>
2017-04-02 09:18:37 -0400

    fukc

--- app.py
+++ app.py
@@ -21,12 +21,13 @@ with open("pat.txt", 'r') as f:
     PAT = tokens[0]
     #print PAT
     verify = tokens[1]
-    #print verify
+    print verify
 
 
 @app.route('/', methods=['GET'])
 def handle_verification():
   print "Handling Verification."
+  print request.args.get('hub.verify_token', '')
   if request.args.get('hub.verify_token', '') == verify:
     print "Verification successful!"
     return request.args.get('hub.challenge', '')
